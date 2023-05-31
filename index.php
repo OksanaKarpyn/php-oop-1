6b73d55b1aaaf49f9a8faacab58ba794100f3c4a
Oksana Karpyn <oksana101992@gmail.com>
2023-05-31 15:24:51 +0200

    agg all'interno della classe costruttore

--- index.php
+++ index.php
@@ -1,16 +1,36 @@
 <?php
 class Movie{
     public $categorie;
-    public $annoProduzione;
+    public $anno_Produzione;
     public $paese;
+    public $nome;
+    // costruttore
+    function  __construct( $_categorie, $_anno_Produzione, $_paese, $_nome ){
+        $this-> categorie = $_categorie;
+        $this-> anno_Produzione = $_anno_Produzione;
+        $this-> paese = $_paese;
+        $this-> nome = $_nome ;
+    }
     
 }
+// modo dinamico
+$film = new Movie( 'horror', 2022, 'USA', 'ANABEL' );
+var_dump($film);
+$serie = new Movie( 'cartone', 2023, 'UA', 'MAVKA' );
+var_dump($serie);
+ 
+// modo statico
+// $film = new Movie();
+// $film -> categorie = 'fantasy';
+// $film -> anno_produzione = '2000';
+// $film -> paese = 'Germany';
 
-$film= new Movie();
-$film -> categorie = 'fantasy';
-$film -> anno_produzione = '2000';
-$film -> paese = 'germany';
-var_dump($film)
+
+// $serie = new Movie();
+// $serie -> categorie = 'dramma';
+// $serie -> anno_produzione = '2010';
+// $serie -> paese = 'Italy';
+
 
 
 ?>
